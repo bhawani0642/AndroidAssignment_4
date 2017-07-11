4fe975206b12b297c392fbab05552202cfdf3d59
bhawani0642 <bhawani0642@gmail.com>
2017-07-11 13:02:05 +0530

    Assignment4

---
+++ app/src/main/java/com/acadgild/loginscreen/MainActivity.java
@@ -0,0 +1,25 @@
+package com.acadgild.loginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.menu.MenuItemImpl;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
+
